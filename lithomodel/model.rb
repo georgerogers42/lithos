0b8de7c150edf4ed941e4976cbb40ec207262785
George Rogers <geo@myhost.(none)>
2011-06-19 15:25:49 -0500

    foo

--- lithomodel/model.rb
+++ lithomodel/model.rb
@@ -1,5 +1,5 @@
 require 'datamapper'
-DataMapper.setup(:default,ENV['DATABASE_URL'] || 'sqlite://blog.db')
+DataMapper.setup(:default,ENV['DATABASE_URL'])
 class Post
   include DataMapper::Resource
   property :id, Serial
